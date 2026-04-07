cd7b0203cd2aef456e7fac8bee39ce265f166afc
hubert.wojcik <hwojcikk@gmail.com>
2026-04-07 19:32:04 +0200

    chore: improve config

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -1,3 +1,20 @@
 module "network" {
     source = "../../modules/network"
 }
+
+module "security_groups" {
+  source = "../../modules/security-groups"
+  vpc_id = module.network.vpc_id
+  allowed_ssh_cidr = var.allowed_ssh_cidr
+}
+
+module "compute" {
+  source = "../../modules/compute"
+  key_name = aws_key_pair.wazuh_lab.key_name
+}
+
+
+resource "aws_key_pair" "wazuh_lab" {
+  key_name   = "wazuh-lab-key"
+  public_key = file("~/.ssh/wazuh-lab-key.pub")
+}
