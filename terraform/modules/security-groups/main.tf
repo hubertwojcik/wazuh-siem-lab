055d0a4c613aa922f0a84083b1179cfabcf68055
hubert.wojcik <hwojcikk@gmail.com>
2026-04-07 18:18:08 +0200

    add rest SG

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -111,12 +111,12 @@ resource "aws_vpc_security_group_egress_rule" "allow_http_to_manager_internet" {
 
 
 # API
-resource "aws_vpc_security_group_ingress_rule" "allow_https_from_internet_api" {
+resource "aws_vpc_security_group_ingress_rule" "allow_3000_from_alb_api" {
     security_group_id = aws_security_group.api_sg.id
 
-    cidr_ipv4 = "0.0.0.0/0"
-    to_port = 443
-    from_port = 443
+    referenced_security_group_id = aws_security_group.alb_sg.id
+    to_port = 3000
+    from_port = 3000
     ip_protocol = "tcp"
 }
 
@@ -129,15 +129,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_internet_api" {
     ip_protocol = "tcp"
 } 
 
-resource "aws_vpc_security_group_egress_rule" "allow_https_to_alb" {
-    security_group_id = aws_security_group.api_sg.id
-
-    referenced_security_group_id = aws_security_group.alb_sg.id
-    to_port = 443
-    from_port = 443
-    ip_protocol = "tcp"
-}
-
 resource "aws_vpc_security_group_egress_rule" "allow_postgres_from_api_database" {
     security_group_id = aws_security_group.api_sg.id    
 
@@ -147,18 +138,36 @@ resource "aws_vpc_security_group_egress_rule" "allow_postgres_from_api_database"
     ip_protocol = "tcp"
 } 
 
+resource "aws_vpc_security_group_egress_rule" "allow_1514_from_api_to_manager" {
+    security_group_id = aws_security_group.api_sg.id
+    
+    referenced_security_group_id = aws_security_group.wazuh_manager_sg.id
+    to_port = 1514
+    from_port = 1514
+    ip_protocol = "tcp"
+} 
+resource "aws_vpc_security_group_egress_rule" "allow_1515_from_api_to_manager" {
+    security_group_id = aws_security_group.api_sg.id
+    
+    referenced_security_group_id = aws_security_group.wazuh_manager_sg.id
+    to_port = 1515
+    from_port = 1515
+    ip_protocol = "tcp"
+} 
+
+
 // Database
 resource "aws_vpc_security_group_ingress_rule" "allow_postgres_from_api" {
     security_group_id = aws_security_group.database_sg.id
     
     referenced_security_group_id = aws_security_group.api_sg.id
-    to_port = 0
-    from_port = 0
-    ip_protocol = "-1"
+    to_port = 5432
+    from_port = 5432
+    ip_protocol = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_api" {
-    security_group_id = aws_security_group.api_sg.id
+    security_group_id = aws_security_group.database_sg.id
     
     referenced_security_group_id = aws_security_group.api_sg.id
     to_port = 22
@@ -166,6 +175,42 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_api" {
     ip_protocol = "tcp"
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_1514_database_manager" {
+    security_group_id = aws_security_group.database_sg.id
+    
+    referenced_security_group_id = aws_security_group.wazuh_manager_sg.id
+    to_port = 1514
+    from_port = 1514
+    ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_1515_database_manager" {
+    security_group_id = aws_security_group.database_sg.id
+    
+    referenced_security_group_id = aws_security_group.wazuh_manager_sg.id
+    to_port = 1515
+    from_port = 1515
+    ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_https_database_internet" {
+    security_group_id = aws_security_group.database_sg.id
+    
+    cidr_ipv4 = "0.0.0.0/0"
+    to_port = 443
+    from_port = 443
+    ip_protocol = "tcp"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_http_database_internet" {
+    security_group_id = aws_security_group.database_sg.id
+    
+    cidr_ipv4 = "0.0.0.0/0"
+    to_port = 80
+    from_port = 80
+    ip_protocol = "tcp"
+}
 
 // ALB
 resource "aws_vpc_security_group_ingress_rule" "allow_https_internet_alb" {
@@ -183,5 +228,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_internet_alb" {
     cidr_ipv4 = "0.0.0.0/0"
     from_port = 80
     to_port = 80
+    ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_3000_alb_api" {
+    security_group_id = aws_security_group.alb_sg.id
+
+    referenced_security_group_id = aws_security_group.api_sg.id
+    from_port = 3000
+    to_port = 3000
     ip_protocol = "tcp"
 }
